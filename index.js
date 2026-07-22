6dbb81c0deb1ef15e6e4a0218cae6c8b3510db10
PHILIP <terhemeniorwua@gmail.com>
2026-07-22 15:17:54 +0100

    complete task

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+/*
+Write JavaScript code that selects a paragraph and an image by their id, then changes the paragraph's text, color, and id, and changes the image's src, alt, width, height, and title attribute.
+*/
+
+let getText = document.querySelector('#text');
+// getText.style.color = 'Green';
+getText.innerHTML = 'Welcome Home';
+getText.id = 'demoText'
+let newId = document.querySelector('#demoText');
+newId.style.color = 'blue'
+newId.style.fontSize = '60px'
+
+
+let getImg = document.querySelector('#img');
+getImg.src = 'img/male.png';
+getImg.style.width = '600px';
+getImg.style.height = '600px';
+getImg.title = 'Man'
