a575c00307241d45371e29afc0b9062e7d941087
PHILIP <terhemeniorwua@gmail.com>
2026-07-26 01:24:09 +0100

    add the update function

--- post.js
+++ post.js
@@ -1,16 +1,17 @@
 // request function
 
+// No 1
 
 let content = document.getElementById('contents')
 let btn = document.querySelector('.btn')
-const newPost = async () =>{
+const newPost = async (id, title, message) =>{
    try{
      let request = await fetch('https://jsonplaceholder.typicode.com/posts', {
         method: "POST",
         body: JSON.stringify({
-            userId: 1,
-            title: 'FIrst request',
-            body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus corporis illo, esse laboriosam, deserunt, id veniam quo natus libero voluptatem fugiat ut doloremque nobis laudantium vitae nulla blanditiis amet sequi.'
+            userId: Number(id),
+            title: String(title),
+            body: String(message)
         }),
         headers: {
             "Content-Type":"application/json; charset=UTF-8"
@@ -24,14 +25,20 @@ const newPost = async () =>{
     let res =  await request.json()
     return res
    } catch(e){
-    console.log(e.message)
-   }
+    document.write(e.message)
+   }console.log(e.message)
 } 
 
-btn.addEventListener('click', ()=>{
-    newPost().then(response =>{
 
-    content.innerHTML = `
+let title = 'First Request';
+let id = 2
+let mssg = 'Loresm iijd dkfs s jfisk fjskkfskff'
+
+
+btn.addEventListener('click', ()=>{
+    newPost(id, title, mssg).then(response =>{
+
+    content.innerHTML += `
     <div class="bg-amber-100 p-5">
     <p class="text-3xl font-bold">${response.userId}</p>
     <h1 class="text-3xl py-2">${response.title}</h1>
@@ -41,3 +48,54 @@ btn.addEventListener('click', ()=>{
 
 })
 })
+
+
+// No 2
+
+let btn2 = document.querySelector('.updBtn')
+
+const updatePost = async (id, title, message) =>{
+   try{
+     let request = await fetch('https://jsonplaceholder.typicode.com/posts/' + id, {
+        method: "PUT",
+        body: JSON.stringify({
+            userId: Number(id),
+            title: String(title),
+            body: String(message)
+        }),
+        headers: {
+            "Content-Type":"application/json; charset=UTF-8"
+        }
+    })
+
+    if(!request.ok){
+        throw new Error(`HTTP Error: ${request.status}`)
+    }
+
+    let res =  await request.json()
+    return res
+   } catch(e){
+    document.write(e.message)
+   }console.log(e.message)
+} 
+
+
+let title1 = 'Second Request';
+let id2 = 3
+let mssg3 = 'Loresm iijd dkfs s jfisk fjskkfskff'
+
+
+  btn2.addEventListener('click', ()=>{
+
+      updatePost(id2, title1, mssg3).then(response =>{
+
+    content.innerHTML += `
+    <div class="bg-amber-100 p-5">
+    <p class="text-3xl font-bold">${response.userId}</p>
+    <h1 class="text-3xl py-2">${response.title}</h1>
+    <p>${response.body}</p>
+    </div>
+    `
+
+})
+  })
