54bde7beb19145f24fa8ee3f80cf683aa4f8031a
PHILIP <terhemeniorwua@gmail.com>
2026-08-03 09:52:05 +0100

    add a textarea and list

--- note.js
+++ note.js
@@ -1,2 +1,34 @@
-let textarea = document.querySelector('form textarea')
-console.log(textarea.value)
+let textarea = document.querySelector('form textarea');
+let noteBtn = document.querySelector('#noteBtn');
+let ul = document.querySelector('ul')
+
+const getNoteValue = () => {
+
+    const note = textarea.value
+
+    const newElement = document.createElement('li');
+
+    newElement.classList.add(
+        'flex',
+        'justify-between',
+        'items-center'
+    )
+
+    const newChild = document.createElement('p')
+    newChild.textContent = note
+    newElement.appendChild(newChild);
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Delete'
+    newElement.appendChild(deleteBtn)
+
+    ul.appendChild(newElement)
+
+    textarea.value = ''
+}
+
+
+
+
+
+noteBtn.addEventListener('click', getNoteValue)
